2c97794ac2cc0916386b174805dea7a778729020
Ahmed Raza Shahid <mr.sherry6967@gmail.com>
2025-03-16 12:14:38 +0500

    counter application

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0);
+  let courseName = "React";
+  let addbtn=document.getElementById("addbtn")
+  let rmbtn=document.getElementById("rmbtn")
+  // let counter = 15;
+
+  const addValue = () => {
+    if (counter>18) {
+      addbtn.setAttribute('disabled','');
+    }
+    else if (counter==0) {
+      rmbtn.removeAttribute('disabled');
+    }
+    counter = counter + 1;
+    setCounter(counter)
+
+  }
+  const removeValue = () => {
+    if (counter<2) {
+      rmbtn.setAttribute('disabled','');
+    }
+    else if (counter==20) {
+      addbtn.removeAttribute('disabled');
+    }
+      counter = counter - 1;
+      setCounter(counter)
+      // rmbtn.removeAttribute('disabled');
+    
+  }
+
+  return (
+    <>
+      <h1>{courseName}</h1>
+      <h1>Counter: {counter}</h1>
+      <button id='addbtn' onClick={addValue}>Add Value {counter}</button><br />
+      <button id='rmbtn' onClick={removeValue}>Remove Value {counter}</button>
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App;

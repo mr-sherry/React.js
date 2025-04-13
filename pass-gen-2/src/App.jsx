7e1517706f7ddf849adcb84b244e45a8c0c2b364
Ahmed Raza Shahid <mr.sherry6967@gmail.com>
2025-04-13 10:21:03 +0500

    update

--- pass-gen-2/src/App.jsx
+++ pass-gen-2/src/App.jsx
@@ -7,36 +7,42 @@ function App() {
   const [randomPass, setRandomPass] = useState("hello");
   const [numBol, setNumBol] = useState(false);
   const [specialBol, setSpecialBol] = useState(false);
+  const [indexGen, setIndexGen] = useState(51);
   const passwordRef = useRef(null);
 
   const genPass = () => {
     let allCaracters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (numBol) allCaracters += "0123456789";
-    if (specialBol) allCaracters += "!@#$%^&*()_+-=[]{}|;:,.<>?/~`\\";
+    if (numBol) {
+      allCaracters += "0123456789";
+      setIndexGen(61);
+    } else if (specialBol) {
+      allCaracters += "!@#$%^&*()_+-=[]{}|;:,.<>?/~`\\";
+      setIndexGen(92);
+    } else {
+      setIndexGen(51);
+    }
 
     let password = "";
     for (let index = 0; index < rangeInput; index++) {
-      const random = Math.floor(Math.random() * allCaracters.length) + 1;
+      const random = Math.floor(Math.random() * indexGen) + 1;
       password = password + allCaracters.charAt(random);
     }
+
     setRandomPass(password);
   };
 
   useEffect(() => {
     genPass();
-  }, [rangeInput, numBol, specialBol]);
+  }, [rangeInput, numBol, specialBol, indexGen]);
 
   let copyToClipboard = () => {
-    let input = passwordRef.current;
-    window.navigator.clipboard.writeText(input.value);
-    input.select();
+    window.Clipboard.select();
   };
-
   return (
     <>
       <h1>Password Generator</h1>
       <input type="text" value={randomPass} ref={passwordRef} />
-      <button onClick={copyToClipboard}>Copy</button>
+      {/* <button onClick={copyToClipboard}>Copy</button> */}
       <br />
       <input
         type="range"
@@ -46,6 +52,8 @@ function App() {
         onChange={(e) => setRangeInput(e.target.value)}
       />
       <p>Pass Lenght: {rangeInput}</p>
+      {/* <input type="checkbox" onClick={(e) => setNumBol(e.target.value)} />
+  Numbers <br /> */}
       <input type="checkbox" onChange={() => setNumBol((prev) => !prev)} />
       Numbers <br />
       <input type="checkbox" onChange={() => setSpecialBol((prev) => !prev)} />
